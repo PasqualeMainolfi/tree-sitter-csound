487123bea2b5d2d314a5b681087990da103f7fef
PasqualeMainolfi <mnlpql@gmail.com>
2025-12-04 13:39:42 +0100

    update arg list confl

--- grammar.js
+++ grammar.js
@@ -162,16 +162,16 @@ module.exports = grammar({
     ),
 
     xout_statement: $ => choice(
-        seq(
+        prec(2, seq(
             'xout',
             '(',
             field('inputs', optional(sep1($.argument_list, ','))),
             ')'
-        ),
-        seq(
+        )),
+        prec(1, seq(
             'xout',
             field('inputs', optional(sep1($.argument_list, ','))),
-        )
+        ))
     ),
 
     header_assignment: $ => seq($.header_identifier, '=', $._expression),
