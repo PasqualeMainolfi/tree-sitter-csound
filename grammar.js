7ac888ab23c71c6e1e4333e3eae87762123743f7
PasqualeMainolfi <mnlpql@gmail.com>
2025-12-06 23:26:34 +0100

    fix ident typed

--- grammar.js
+++ grammar.js
@@ -118,7 +118,7 @@ module.exports = grammar({
       'endop'
     ),
 
-    legacy_udo_args: $ => token(/[a-zA-Z0-9](\[\])*/),
+    legacy_udo_args: $ => token(/[a-zA-Z0-9\[\]]+/),
     _void: $ => token('void'),
 
     modern_udo_inputs: $ => seq(
